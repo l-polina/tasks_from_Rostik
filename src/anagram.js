6248b1be746db7f2f4e896f31580867ca3a0d48a
Polina <polina.liutsko@gmail.com>
2020-07-25 13:38:43 +0300

    covered tasks with tests

--- src/anagram.js
+++ src/anagram.js
@@ -2,4 +2,4 @@ function isAnagram(str1, str2) {
   return str1.split("").sort().join("") === str2.split("").sort().join("");
 }
 
-console.log(isAnagram("afbcd", "dafbc"));
+module.exports = isAnagram;
--- src/palindrome.js
+++ src/palindrome.js
@@ -2,4 +2,4 @@ function isPalindrome(str) {
   return str === str.split("").reverse().join("");
 }
 
-console.log(isPalindrome("aba"));
+module.exports = isPalindrome;
